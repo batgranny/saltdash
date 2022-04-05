751ce6b6ea313517b6c7c69f90399b6481b23c94
Chris McAllister <chris.mcallister@imanage.com>
2022-04-05 18:05:57 +0100

    ini change

--- php/src/index.php
+++ php/src/index.php
@@ -1,22 +1,55 @@
 <?php
 include 'dbconn.php';
-$searchres = $_GET['search'];
+// to use this run the docker-compose file at /Users/chrismcallister/Documents/docker
+//from a browser http://localhost 8000
+ini_set('memory_limit', '-1');
 
-if ($searchres == "Success"){
-    $searchres = "0";
-}elseif ($searchres == "Fail"){
-    $searchres = "1";
-
-}
-
-$sql = "SELECT * from salt_returns WHERE id LIKE '%".$searchres."%' UNION SELECT * from salt_returns WHERE fun LIKE '%".$searchres."%' UNION SELECT * from salt_returns WHERE jid LIKE '%".$searchres."%' UNION SELECT * from salt_returns WHERE alter_time LIKE '%".$searchres."%' UNION SELECT * from salt_returns WHERE success LIKE '%".$searchres."%'
-";
+  
+// select query
+$sql = "SELECT * FROM salt_returns WHERE fun NOT LIKE 'runner%' ORDER BY jid DESC LIMIT 40";
 
 if ($result = $conn->query($sql)) {
     while ($data = $result->fetch_object()) {
         $users[] = $data;
     }
 }
+
+$sql = "SELECT * FROM salt_returns WHERE success = '1'";
+if ($result2 = $conn->query($sql)) {
+    while ($data2 = $result2->fetch_object()) {
+        $users2[] = $data2;
+    }
+}
+
+if (empty($users2)){
+  $goodqs = "No data";
+}else{
+  $goodqs = count($users2);
+}
+
+$sql = "SELECT * FROM salt_returns WHERE success = '0'";
+if ($result3 = $conn->query($sql)) {
+    while ($data3 = $result3->fetch_object()) {
+        $users3[] = $data3;
+    }
+}
+
+if (empty($users3)){
+  $badqs = "No data";
+}else{
+  $badqs = count($users3);
+}
+
+if (empty($users2)){
+  $goodqs = "No data";
+}else{
+  $goodqs = count($users2);
+}
+
+//Gauge stuff
+$total = (int)$goodqs + (int)$badqs;
+$gooddec = round((int)$goodqs / $total, 2);
+$baddec = round((int)$badqs / $total, 2);
 ?>
 
 <!doctype html>
@@ -31,7 +64,7 @@ if ($result = $conn->query($sql)) {
     <link rel="icon" href="../../favicon.ico">
 
     <title>Saltdash</title>
-
+    <link href="/css/gauge.css" rel="stylesheet">
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -54,60 +87,48 @@ if ($result = $conn->query($sql)) {
 
   <body>
     <?php  include 'nav.php'; ?>
-   <? ini_set('memory_limit', '-1'); ?>
   
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
           <h1 class="page-header">Dashboard</h1>
 
           <div class="row placeholders">
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <div class="panel panel-success">
+            <div class="col-xs-6 col-sm-6 placeholder">
+              <div class="panel panel-default">
                 <div class="panel-heading">
-                  <h3 class="panel-title">Successfull Queries</h3>
+                  <h3 class="panel-title">Successfull Jobs</h3>
                 </div>
                   <div class="panel-body">
-                  <h1>135</h1>
+                  <div class="gauge">
+  <div class="gauge__body">
+    <div class="gauge__fill"></div>
+    <div class="gauge__cover"></div>
+  </div>
+</div>
                   </div>
             </div>
             </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-            <div class="panel panel-danger">
+            <div class="col-xs-6 col-sm-6 placeholder">
+            <div class="panel panel-default">
                 <div class="panel-heading">
-                  <h3 class="panel-title">Failed Queries</h3>
+                  <h3 class="panel-title">Failed Jobs</h3>
                 </div>
                   <div class="panel-body">
-                  <h1>25</h1>
+                  <div class="gauge2">
+  <div class="gauge__body2">
+    <div class="gauge__fill2"></div>
+    <div class="gauge__cover2"></div>
+  </div>
+</div>
                   </div>
             </div>
             </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-            <div class="panel panel-success">
-                <div class="panel-heading">
-                  <h3 class="panel-title">Successfull Queries</h3>
-                </div>
-                  <div class="panel-body">
-                  <h1>135</h1>
-                  </div>
-            </div>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-            <div class="panel panel-success">
-                <div class="panel-heading">
-                  <h3 class="panel-title">Successfull Queries</h3>
-                </div>
-                  <div class="panel-body">
-                  <h1>135</h1>
-                  </div>
-            </div>
-            </div>
-          </div>
-
-          <h3 class="sub-header">Search results for <? echo $searchres ?></h3>
+</div>
+          <h2 class="sub-header">Recent Jobs</h2>
           <div class="row">
             <div class="col-lg-9">
             </div>
             <div class="col-lg-3">
-            <form action="/test.php">
+            <form action="/results.php">
               <div class="input-group">
                 <input type="text" class="form-control" placeholder="Search Jobs..." name="search">
                 <span class="input-group-btn">
@@ -161,6 +182,36 @@ if ($result = $conn->query($sql)) {
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../../dist/js/bootstrap.min.js"></script>
+    <script>
+      const gaugeElement = document.querySelector(".gauge");
+
+function setGaugeValue(gauge, value) {
+  if (value < 0 || value > 1) {
+    return;
+  }
+
+  gauge.querySelector(".gauge__fill").style.transform = `rotate(${
+    value / 2
+  }turn)`;
+  gauge.querySelector(".gauge__cover").textContent = `<? echo "$goodqs" ?>`;
+}
+
+const gaugeElement2 = document.querySelector(".gauge2");
+
+function setGaugeValue2(gauge2, value) {
+  if (value < 0 || value > 1) {
+    return;
+  }
+
+  gauge2.querySelector(".gauge__fill2").style.transform = `rotate(${
+    value / 2
+  }turn)`;
+  gauge2.querySelector(".gauge__cover2").textContent = `<? echo "$badqs" ?>`;
+}
+
+setGaugeValue(gaugeElement, <? echo "$gooddec" ?>);
+setGaugeValue2(gaugeElement2, <? echo "$baddec" ?>);
+    </script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
     <script src="../../assets/js/vendor/holder.min.js"></script>
     <script src="https://kit.fontawesome.com/f8c057d86e.js" crossorigin="anonymous"></script>
